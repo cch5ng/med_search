d93eca88b5c05a08116c8df870493f2a0d138f8f
Carol Chung <cch5ng@gmail.com>
2017-11-11 14:37:30 -0800

    fix: remove link from last view search results list

--- client/modules/Post/components/SearchResultsList.js
+++ client/modules/Post/components/SearchResultsList.js
@@ -1,28 +1,41 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { Link } from 'react-router-dom';
 
 // Import Components
 //import PostListItem from './PostListItem/PostListItem';
 
-function SearchResultsList(props) {
-  return (
-    <div className="listView">
-      {
-        props.data.map(med => (
-          <div key={med['rxcui']} className=''>
-            <h3 className=''>
-              <Link to={`/search/${props.drug1}/${med['rxcui']}`} >
-                {med['name']}
-              </Link>
-            </h3>
-            <p className=''>Synonym: {med['synonym']}</p>
-            <p className=''>Suppress: {med['suppress']}</p>
-            <hr className='' />
-          </div>
-        ))
-      }
-    </div>
-  );
+class SearchResultsList extends Component {
+
+  displayNameOrLink(med) {
+    if (this.props.needLink === "false") {
+      return (`${med['name']}`)
+    }
+    return (
+      <Link to={`/search/${this.props.drug1}/${med['rxcui']}`} >
+        {med['name']}
+      </Link>
+    )
+  }
+
+  render() {
+    return (
+      <div className="listView">
+        {
+          this.props.data.map(med => (
+            <div key={med['rxcui']} className=''>
+              <h3 className=''>
+                {this.displayNameOrLink(med)}
+              </h3>
+              <p className=''>Synonym: {med['synonym']}</p>
+              <p className=''>Suppress: {med['suppress']}</p>
+              <hr className='' />
+            </div>
+          ))
+        }
+      </div>
+    )  
+  }
+
 }
 
 export default SearchResultsList;
