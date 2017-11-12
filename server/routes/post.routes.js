3d8efbd43fb15753d01be8a7827e7d8303e89beb
Carol Chung <cch5ng@gmail.com>
2017-11-11 22:24:14 -0800

    cleanup start

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -6,12 +6,12 @@ const router = new Router();
 router.route('/posts').get(PostController.getPosts);
 
 // Get one post by cuid
-router.route('/posts/:cuid').get(PostController.getPost);
+//router.route('/posts/:cuid').get(PostController.getPost);
 
 // Add a new Post
 router.route('/posts').post(PostController.addPost);
 
 // Delete a post by cuid
-router.route('/posts/:cuid').delete(PostController.deletePost);
+//router.route('/posts/:cuid').delete(PostController.deletePost);
 
 export default router;
