20c9a4a8ade174da8b51b0faf59392fe3fa710b7
Carol Chung <cch5ng@gmail.com>
2017-11-11 22:23:19 -0800

    fix: add dummy popular query data

--- server/dummyData.js
+++ server/dummyData.js
@@ -3,41 +3,16 @@ import Post from './models/post';
 export default function () {
   Post.count().exec((err, count) => {
     if (count > 0) {
-      return;
+     return;
     }
 
-    const content1 = `Sed ut perspiciatis unde omnis iste natus error
-      sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-      eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
-      vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-      aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-      qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
-      ipsum quia dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-      enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum`;
+    const post1 = new Post({ name: 'Acetaminophen', id: '161', cuid: 'cikqgkv4q01ck7453ualdn3hd', frequency: 12001 });
+    const post2 = new Post({ name: 'Chlorpheniramine', id: '2400', cuid: '7453ualdn3hfcikqgkv4q01ck', frequency: 12002 });
+    const post3 = new Post({ name: 'Dextromethorphan', id: '3289', cuid: 'cikqdn3hdgkv4q01ck7453ual', frequency: 12003 });
+    const post4 = new Post({ name: 'Phenylephrine', id: '8163', cuid: '3hfcikqgkv4q01ck7453ualdn', frequency: 12004 });
+    const post5 = new Post({ name: 'Doxylamine', id: '3642', cuid: 'n3hdcikqgkv4q01ck7453uald', frequency: 12000 });
 
-    const content2 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-      enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum. Sed ut perspiciatis unde omnis iste natus error
-      sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-      eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
-      vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-      aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-      qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
-      ipsum quia dolor sit amet.`;
-
-    const post1 = new Post({ name: 'Admin', title: 'Hello MERN', slug: 'hello-mern', cuid: 'cikqgkv4q01ck7453ualdn3hd', content: content1 });
-    const post2 = new Post({ name: 'Admin', title: 'Lorem Ipsum', slug: 'lorem-ipsum', cuid: 'cikqgkv4q01ck7453ualdn3hf', content: content2 });
-
-    Post.create([post1, post2], (error) => {
+    Post.create([post1, post2, post3, post4, post5 ], (error) => {
       if (!error) {
         // console.log('ready to go....');
       }
