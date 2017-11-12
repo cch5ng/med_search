18196630c862277f62a47d8cb3951faab1e07f35
Carol Chung <cch5ng@gmail.com>
2017-11-12 13:54:49 -0800

    refactor: reset init freq to 1

--- server/dummyData.js
+++ server/dummyData.js
@@ -15,20 +15,20 @@ export default function () {
     const post2 = new Post({ name: "Acetaminophen 500 MG / Caffeine 65 MG Disintegrating Oral Tablet [Excedrin Quick Tab]",
       synonym: "APAP 500 MG / Caffeine 65 MG Disintegrating Oral Tablet [Excedrin Quick Tab]", 
       rxcui: "702615", 
-      frequency: 2 
+      frequency: 1 
     });
     const post3 = new Post({ name: "Acetaminophen 325 MG / Guaifenesin 200 MG Oral Capsule [Comtrex Deep Chest Cold Non Drowsy]",
       synonym: "APAP 325 MG / Guaifenesin 200 MG Oral Capsule [Comtrex Deep Chest Cold Non Drowsy]", 
       rxcui: "707199",
-      frequency: 3 });
+      frequency: 1 });
     const post4 = new Post({ name: "Acetaminophen 500 MG / Codeine Phosphate 60 MG Oral Tablet [Codrix]",
       synonym: "Codrix 500/60 Oral Tablet",
       rxcui: "994051",
-      frequency: 4 });
+      frequency: 1 });
     const post5 = new Post({ name: "Acetaminophen 32 MG/ML / Chlorpheniramine Maleate 0.2 MG/ML / Dextromethorphan Hydrobromide 1 MG/ML Oral Solution",
       synonym: "acetaminophen 160 MG / chlorpheniramine maleate 1 MG / dextromethorphan HBr 5 MG per 5 ML Oral Solution",
       rxcui: "1038876",
-      frequency: 5 });
+      frequency: 1 });
 
     Post.create([post1, post2, post3, post4, post5 ], (error) => {
       if (!error) {
