9d8e72ad36fb30fa25ea973bcee1cd88adec1ed0
Carol Chung <cch5ng@gmail.com>
2017-11-12 10:22:08 -0800

    content: remove identifier

--- client/modules/App/components/Footer/Footer.js
+++ client/modules/App/components/Footer/Footer.js
@@ -10,7 +10,7 @@ import bg from '../../header-bk.png';
 export function Footer() {
   return (
     <div style={{ background: `#FFF url(${bg}) center` }} className={styles.footer}>
-      <p>2017 &middot; Canvas Medical</p>
+      <p>2017 &middot; Your Company</p>
       <p><a href="https://github.com/cch5ng" target="_blank">github &middot; @cch5ng</a></p>
     </div>
   );
